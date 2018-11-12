1890595ee40b4b0a51d0ea7f66bdde2921c6300a
Donatas Repecka <donatasrep@gmail.com>
2018-11-12 18:47:28 +0200

    Position embedding

--- run_pretraining.py
+++ run_pretraining.py
@@ -380,7 +380,7 @@ def _decode_record(record, max_seq_length, max_predictions_per_seq):
     # ## TODO: think if empty spaces also include in masked ids
     # tf_positions_to_mask = tf.convert_to_tensor(positions_to_mask)
 
-    tf_positions_to_mask = tf.constant([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+    tf_positions_to_mask = tf.convert_to_tensor([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 
     example["masked_lm_positions"] = tf_positions_to_mask
     example["masked_lm_ids"] = tf.gather(example["input_ids"], tf_positions_to_mask)
