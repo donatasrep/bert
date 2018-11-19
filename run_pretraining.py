9fa787b29c2007156b2a25cfc0de342073c83308
Donatas Repecka <donatasrep@gmail.com>
2018-11-19 16:01:18 +0200

    Add printing

--- run_pretraining.py
+++ run_pretraining.py
@@ -150,7 +150,7 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
             masked_lm_positions, masked_lm_ids, masked_lm_weights)
         print("!!!!!!!!!!!!!!!!!!!!!!!")
         print(masked_lm_log_probs.shape)
-        masked_lm_loss = tf.Print(masked_lm_loss, [tf.argmax(masked_lm_log_probs, axis=1, output_type=tf.int32)], "Guess:", summarize=128)
+        masked_lm_loss = tf.Print(masked_lm_loss, [tf.argmax(tf.reshape(masked_lm_log_probs, [16, 50, 22])[0], axis=1, output_type=tf.int32)], "Guess:", summarize=128)
         masked_lm_loss = tf.Print(masked_lm_loss, [masked_lm_ids[0]], "Correct:", summarize=128)
         masked_lm_loss = tf.Print(masked_lm_loss, [masked_lm_log_probs[0]], "Probabs:", summarize=128)
         masked_lm_loss = tf.Print(masked_lm_loss, [input_ids[0]], "input:", summarize=512)
