048e84c1c16d72902cfdb23f919462cff4461fc9
Donatas Repecka <donatasrep@gmail.com>
2018-12-14 20:45:01 +0200

    Added local path to hook

--- eval_results_hook.py
+++ eval_results_hook.py
@@ -8,14 +8,15 @@ class EvalResultsHook(SessionRunHook):
 
     def __init__(self, args_to_store, output_dir):
         self.args_to_store = list([args_to_store[i] for i in [5,7,8]])
-        filename = datetime.now().strftime("%Y%m%d-%H%M%S")
-        # os.makedirs(os.path.join("weight", "eval_full"), exist_ok=True)
-        self.filename = os.path.join("weights", "eval_full", filename+".npy")
+
 
     def before_run(self, run_context):  # pylint: disable=unused-argument
         return SessionRunArgs(self.args_to_store)
 
     def after_run(self, run_context, run_values):
+        filename = datetime.now().strftime("%Y%m%d-%H%M%S")
+        # os.makedirs(os.path.join("weight", "eval_full"), exist_ok=True)
+        filename = os.path.join("weights", "eval_full", filename+".npy")
         values = run_values.results
-        with open(self.filename, 'wb') as f:
+        with open(filename, 'wb') as f:
             numpy.save(f, values)
