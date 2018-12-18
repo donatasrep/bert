a635274a2fd7a843732b2d2d04d007c36cb07e6a
Donatas Repecka <donatasrep@gmail.com>
2018-12-18 17:48:54 +0200

    Improved eval

--- run_pretraining.py
+++ run_pretraining.py
@@ -216,12 +216,12 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
 
             n_predictions = masked_lm_ids.get_shape().as_list()[-1]
             probs = tf.reshape(masked_lm_log_probs,
-                               [1024, n_predictions, bert_config.vocab_size])
+                               [-1, n_predictions, bert_config.vocab_size])
             masked_lm_predictions = tf.argmax(probs, axis=-1, output_type=tf.int32)
             correct_prediction = tf.equal(masked_lm_predictions, masked_lm_ids)
             masked_lm_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), axis=1)
             # tf.summary.scalar("train_accuracy", tf.reduce_mean(masked_lm_accuracy))
-            loss_per_seq = tf.reduce_mean(tf.reshape(masked_lm_example_loss, [1024, n_predictions]), axis=1)
+            loss_per_seq = tf.reduce_mean(tf.reshape(masked_lm_example_loss, [-1, n_predictions]), axis=1)
 
             # eval_metrics = (metric_fn, [masked_lm_example_loss, masked_lm_log_probs, masked_lm_ids,
             #                             masked_lm_weights])
@@ -236,7 +236,8 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
                                            loss=total_loss,
                                            eval_metrics=eval_metrics,
                                            scaffold_fn=scaffold_fn,
-                                           evaluation_hooks=[eval_hook(variables_to_export, FLAGS.output_dir)])
+                                           #evaluation_hooks=[eval_hook(variables_to_export, FLAGS.output_dir)]
+            )
         else:
             raise ValueError("Only TRAIN and EVAL modes are supported: %s" % (mode))
 
